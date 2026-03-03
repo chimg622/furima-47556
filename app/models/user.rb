cffce5fab59776e3f65278a3f7a641f5c2bfba5b
chimg622 <chimg622@gmail.com>
2026-03-03 14:08:29 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :purchases
+  # has_many :items
+  # has_many :purchases
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
   validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
