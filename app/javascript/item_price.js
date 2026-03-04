830b5c1532b6ac13f1c07556e4b31e8801f02ec5
chimg622 <chimg622@gmail.com>
2026-03-04 18:37:49 +0900

    商品出品機能実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxPrice = document.getElementById("add-tax-price");
+    addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
+
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1));
+  })
+};
+
+window.addEventListener('turbo:load', price);
+window.addEventListener('turbo:render', price);
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_scheduled
+
+  belongs_to :user
+  has_many :purchases
+  has_one_attached :image
+
+  validates :image, :name, :info, :price, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_scheduled_id, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_scheduled_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300, 
+    less_than_or_equal_to: 9_999_999 
+  }
 end
+
