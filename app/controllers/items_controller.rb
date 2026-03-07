8434c6748c1738555bdb492279fa995abd110a61
chimg622 <chimg622@gmail.com>
2026-03-07 18:25:15 +0900

    コントローラーファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")  
@@ -10,20 +11,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -46,4 +44,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :info, :price, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_scheduled_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
   end
+
+end
